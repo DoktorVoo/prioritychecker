89407c182e5fd19e49cf68fd8f464517db6b2c03
Der Doktor <thepraxis@gmx.de>
2026-03-13 09:55:14 +0100

    Überarbeitete Dateien aktualisiert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
 
     // Compose BOM – keeps all versions in sync
     implementation(platform(libs.androidx.compose.bom))
